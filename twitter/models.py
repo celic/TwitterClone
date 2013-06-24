c0d0496d404a600e4c044aa86c59a23a50f881dc
celic <cceli@codequarry.net>
2013-06-23 20:04:21 -0400

    Adds twitter bootstrap

--- twitter/models.py
+++ twitter/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class User(models.Model):
+class User(models.User):
 	name = models.CharField(max_length = 50)
 	email = models.EmailField(max_length = 75)
 	password = models.CharField(max_length = 20)
